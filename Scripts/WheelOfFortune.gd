8b22c11b17f714772d0efef8bb24731d4da9213c
josprachi <josprachi@pop-os.localdomain>
2020-09-25 02:06:11 +0530

    finally geot the values of colliding bodies

--- Scripts/WheelOfFortune.gd
+++ Scripts/WheelOfFortune.gd
@@ -12,5 +12,21 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	print(self.angular_velocity)
+#func _process(delta):
+	#print(self.angular_velocity)	#if floor(self.angular_velocity)<=0 :		#print(self.angular_velocity)
+	
+	
+
+
+func _on_Timer_timeout():
+	if ( self.angular_velocity==0.000000) :
+		self.angular_velocity=100.0 #pass # Replace with function body.
+
+
+
+func _on_WheelPin_body_entered(body):
+	#print(self.angular_velocity)
+	if self.angular_velocity<=0.2 :
+		#print("XXX")
+		if(body.get_name().begins_with ("Petal")):
+			print(body.PetalColorName)
