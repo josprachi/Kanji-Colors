14f79f5b7f73e0ef04ec21230fd50e8b24027909
josprachi <josprachi@pop-os.localdomain>
2020-09-28 20:49:04 +0530

    get button name

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -10,18 +10,18 @@ var posArray:= [0,1,2,3,4,5,6,7,8,9]
 #onready var tween := get_node("Tween")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print($CoverSprite.get_rect().size.y)
+	#print($CoverSprite.get_rect().size.y)
 	coverPos=$CoverSprite.position
 	coverFinalPos=Vector2($CoverSprite.position.x,coverPos.y+($CoverSprite.get_rect().size.y*4))#texture.get_size().y)
+	var index=0
+	for button in get_tree().get_nodes_in_group("ColorButtons"):
+		button.connect("pressed", self, "_which_button_pressed", [button])
+
+func _which_button_pressed(button):
+	get_parent().get_node("ColorWheel").checkButtonName(button)	
+	#var button_name = button.name
+	#print(button_name)
 	
-	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func show_Buttons():
 	$Tween.interpolate_property($CoverSprite, "position",coverPos, coverFinalPos, 0.5,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Tween.start()
@@ -39,16 +39,12 @@ func set_SpinButton_Text():
 	$SpinButton.texture_normal=tex
 	
 func _on_SpinButton_pressed():
-	#print(get_parent().get_node("SpinWheel").state)#get_child(0).state)
-	#if get_parent().get_node("SpinWheel").state =="idle":
-		#print("idle")
+	
 	posArray.shuffle()
 	var index=0
-	print(posArray)
+	#print(posArray)
 	while index<$ButtonRect/ButtonArray.get_child_count():		
 		$ButtonRect/ButtonArray.move_child($ButtonRect/ButtonArray.get_children()[index],posArray[index])
 		index+=1
-	#$ButtonRect/ButtonArray.move_child($ButtonRect/ButtonArray.get_children()[0],2)
-	#print($ButtonRect/ButtonArray.get_child(0))
 	show_Buttons()
-	#print("spin button pressed")
+	
--- Scripts/WheelOfFortune.gd
+++ Scripts/WheelOfFortune.gd
@@ -4,29 +4,27 @@ extends RigidBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var rng
+var petalColor 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#print(self.angular_velocity)	#if floor(self.angular_velocity)<=0 :		#print(self.angular_velocity)
-	
-	
+	rng = RandomNumberGenerator.new()
+	rng.randomize()
 
 
 func _on_Timer_timeout():
-	if ( self.angular_velocity==0.000000) :
-		self.angular_velocity=100.0 #pass # Replace with function body.
+	if ( self.angular_velocity==0.0) :
+		self.angular_velocity=rng.randf_range(40.0, 150.0) #pass # Replace with function body.
 
-
+func checkButtonName(button):
+	print("here1")
+	print(button.name)
+	print("here2")
 
 func _on_WheelPin_body_entered(body):
 	#print(self.angular_velocity)
-	if self.angular_velocity<=0.2 :
-		#print("XXX")
+	if self.angular_velocity<1 :		
 		if(body.get_name().begins_with ("Petal")):
 			print(body.PetalColorName)
+			self.angular_velocity=0.0
